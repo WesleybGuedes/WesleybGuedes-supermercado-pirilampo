ab686bc99479f66733427ba59ba1a4e60aaf2713
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-06-12 18:05:32 -0300

    requisito 5 almost

--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -1,7 +1,13 @@
 const stockProducts = require('./data.json');
 
 const getProductsAmount = () => {
-  // Desenvolva seu código dentro dessa função...
+  const fullStock = undefined;
+  let stockPerProducts = [];
+    for ( index = 0; index < stockProducts.length; index += 1 ) {
+      const elemento = stockProducts[index];
+      stockPerProducts[index] = elemento.quantityInStock;
+    }
+  return stockPerProducts;
 };
-
+console.log(getProductsAmount());
 module.exports = { getProductsAmount };
