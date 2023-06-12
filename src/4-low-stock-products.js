6b9f491a7596035f7c75058991535085877004b5
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-06-12 17:46:41 -0300

    requisito 4 done

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -2,17 +2,12 @@ const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
   const lowStock = [];
-  const quantityStock = [];
-  for ( index = 0; index < stockProducts.length; index += 1 ) {
+    for ( index = 0; index < stockProducts.length; index += 1 ) {
     const elemento = stockProducts[index];
     if (elemento.quantityInStock > 0 && elemento.quantityInStock <= 10) {
-      lowStock.push(elemento.productName);
-      quantityStock.push(elemento.quantityInStock);
+      lowStock.push(`${elemento.productName}: ${elemento.quantityInStock} unidades`);
     };
   };
   return lowStock;
 };
-console.log(getLowStockProducts());
 module.exports = { getLowStockProducts };
-/* O formato dos elementos do array deve ser: 
-{nome do produto}: {quantidade em estoque} unidades; */
