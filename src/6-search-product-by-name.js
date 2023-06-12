82e7876caea08a84194f84c20b7c5c58d1f62b71
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-06-12 20:16:14 -0300

    requisito 6 done

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -3,16 +3,14 @@ const stockProducts = require('./data.json');
 const searchProductByName = (searchProduct) => {
     for ( index = 0; index < stockProducts.length; index += 1 ) {
     const elemento = stockProducts[index];
-    let formattedPrice = elemento.price;
       if (searchProduct === elemento.productName) {
-      return (`${elemento.description} R$ ${formattedPrice}`);
-      
-    }
-        else if (searchProduct != elemento.productName) {
-          return null;
-    }
-  }
+      return {
+        description: elemento.description,
+        formattedPrice: `R$ ${elemento.price.toFixed(2)}`
+      };      
+    };
+  };
+    return null;
 };
-console.log(searchProductByName('Cenoura'));
 
 module.exports = { searchProductByName };
