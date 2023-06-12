bd535483bb1384be927b09ebd2d84ec8e7d354d3
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-06-12 15:56:25 -0300

    3 requisito done

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -1,7 +1,13 @@
 const stockProducts = require('./data.json');
 
 const getOutOfStockProducts = () => {
-  // Desenvolva seu código dentro dessa função...
+  const outOfStock = [];
+  for ( index = 0; index < stockProducts.length; index += 1 ) {
+    const elemento = stockProducts[index];
+    if (elemento.quantityInStock === 0) {
+      outOfStock.push(elemento.productName);
+    };
+  };
+  return outOfStock;
 };
-
 module.exports = { getOutOfStockProducts };
