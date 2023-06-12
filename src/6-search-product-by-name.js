6ad1527ec8ec313d10267d399e670205b046b2fa
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-06-12 19:28:54 -0300

    requisito 6 almost

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,18 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductByName = (searchProduct) => {
+    for ( index = 0; index < stockProducts.length; index += 1 ) {
+    const elemento = stockProducts[index];
+    let formattedPrice = elemento.price;
+      if (searchProduct === elemento.productName) {
+      return (`${elemento.description} R$ ${formattedPrice}`);
+      
+    }
+        else if (searchProduct != elemento.productName) {
+          return null;
+    }
+  }
 };
+console.log(searchProductByName('Cenoura'));
 
 module.exports = { searchProductByName };
