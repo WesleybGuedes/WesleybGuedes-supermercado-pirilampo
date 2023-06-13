f28e6181486be785c1c27acf6f64a1a028dd41c1
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-06-12 21:13:22 -0300

    requisito 7 problem

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,16 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductsByBrand = (searchBrand) => {
+   for ( index = 0; index < stockProducts.length; index += 1 ) {
+    const elemento = stockProducts[index];
+      if (searchBrand === elemento.brand) {
+      return [{
+        description: elemento.description,
+        formattedPrice: `R$ ${elemento.price}`
+      }];      
+    };
+  };
+    return [];
 };
-
+/* console.log(searchProductsByBrand("Hortifruti")); */
 module.exports = { searchProductsByBrand };
