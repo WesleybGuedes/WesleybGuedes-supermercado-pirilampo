8ad513fd7bb83a49b354d950ec41fdad789dface
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-06-13 00:17:13 -0300

    10 resolvido na marra

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -4,11 +4,14 @@ const getProductsRichInVitamin = () => {
   const productsVitamin = [];
   for ( index = 0; index < stockProducts.length; index += 1 ) {
     const elemento = stockProducts[index];
-      if (elemento.nutritionalInfo) {
-      let alimentArray = {
+    const elementoVitamin = elemento.nutritionalInfo.vitamins;
+      if (elemento.nutritionalInfo.vitamins != undefined) {  
+        let vitaminsProduct = Object.keys(elementoVitamin);  //pq so funciona dentro do if
+        let qntVitaminsProduct = Object.values(elementoVitamin);  //pq so funciona dentro do if
+        let alimentArray = {
         description : elemento.description,
         formattedPrice : `R$ ${elemento.price}`,
-        vitaminsInformation : []    
+        vitaminsInformation : [`${vitaminsProduct} - ${qntVitaminsProduct}`],     
       };
       productsVitamin.push(alimentArray);
  
