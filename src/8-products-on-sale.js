2d8bad772675b1eb08e22cfd8d3fe694ec37e80d
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-06-12 21:39:30 -0300

    requisito 8 problem

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -1,7 +1,20 @@
 const stockProducts = require('./data.json');
 
-const getProductsOnSale = () => {
-  // Desenvolva seu código dentro dessa função...
+const getProductsOnSale = (promotional) => {
+  const productsSale = {};
+  const promotionalProduct = [];
+  for ( index = 0; index < stockProducts.length; index += 1 ) {
+    const elemento = stockProducts[index];
+      if (promotional === elemento.productName) {
+      return [
+        productsSale.description = elemento.description,
+        productsSale.formattedPrice = `R$ ${elemento.price}`,
+        productsSale.onSale = elemento.onSale ];      
+    };
+ 
+  };
+     return promotionalProduct;
 };
+
 
 module.exports = { getProductsOnSale };
