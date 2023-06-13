4a284a61b3617184467a0f52c52af6ab446d5918
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-06-12 23:11:28 -0300

    requisito 9 fixed

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -4,11 +4,11 @@ const getProductsWithAllergyOrIntoleranceInfo = () => {
   const productsAlergyInt = [];
   for ( index = 0; index < stockProducts.length; index += 1 ) {
     const elemento = stockProducts[index];
-      if (elemento.allergyOrIntolerance) {
+      if (elemento.allergyOrIntolerance != undefined) {
       let alimentArray = {
         description : elemento.description,
         formattedPrice : `R$ ${elemento.price}`,
-        allergyOrIntolerance : `Pode conter ${elemento.allergyOrIntolerance}.`      
+        allergyOrIntoleranceMessage : `Pode conter: ${elemento.allergyOrIntolerance.join(' ')}`      
       };
     productsAlergyInt.push(alimentArray);
  
